b5ee1f6efc2e783b3fd511ae24c0269b0d929213
Sktyagi007 <sarthak765888@gmail.com>
2022-03-21 19:43:26 +0530

    test 1

--- test.js
+++ test.js
@@ -9,18 +9,16 @@
 // let a =2;{let a =3;{let a=4;{let a = 5; console.log(a);}console.log(a);}console.log(a);}console.log(a);
 
 // Q3
-// let arr = [ { name: "Delhi", rainfall: [2.3, 4, 3.1, 5.5, 1.1, 1.2, 7] }, { name: "Noida", rainfall: [6.3, 0, 0.1, 3.5, 1, 2.6, 0.7] }, { name: "Dehradun", rainfall: [12, 5.6, 3.1, 0.55, 11, 16.2, 19] }, { name: "Nanital", rainfall: [8, 1.4, 0.61, 15.5, 6.6, 2, 9.82] }, ]
-
-// for(let  i =0 ; i<arr.length; i++){
-//     // console.log(arr[i]);
-//     for(key in arr[i]){
-//         let newArr = arr[i][key];
-//         // console.log(newArr);
-//         for(let j = 0; j<newArr.length; j++){
-//             console.log(newArr[j]);
-//         }
-//     }
-// }
+let arr = [ { name: "Delhi", rainfall: [2.3, 4, 3.1, 5.5, 1.1, 1.2, 7] }, { name: "Noida", rainfall: [6.3, 0, 0.1, 3.5, 1, 2.6, 0.7] }, { name: "Dehradun", rainfall: [12, 5.6, 3.1, 0.55, 11, 16.2, 19] }, { name: "Nanital", rainfall: [8, 1.4, 0.61, 15.5, 6.6, 2, 9.82] }, ]
+let sum = 0;
+let count = 0;
+for(let  i =0 ; i<arr.length; i++){
+    for(let j = 0; j<arr[i].rainfall; j++){
+        sum = sum+rainfall[j];
+        count = count+1;
+    }
+    console.log(sum)
+}
 // [ { name: "Roorkee", rainfall: [5, 6, 5, 5, 4, 7, 8] }, { name: "Pauri", rainfall: [3, 3, 3, 1, 2, 2, 2] }, ];
 // function rainDance(rainfall) {
 //     let reqAns = [];
