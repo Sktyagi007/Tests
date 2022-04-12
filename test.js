62e3f47885d77adab1f7a2857cda76d94ed4ec8c
Sktyagi007 <sarthak765888@gmail.com>
2022-04-12 22:10:38 +0530

    Q3 completed

--- test.js
+++ test.js
@@ -9,33 +9,25 @@
 // let a =2;{let a =3;{let a=4;{let a = 5; console.log(a);}console.log(a);}console.log(a);}console.log(a);
 
 // Q3
-let arr = [ { name: "Delhi", rainfall: [2.3, 4, 3.1, 5.5, 1.1, 1.2, 7] }, { name: "Noida", rainfall: [6.3, 0, 0.1, 3.5, 1, 2.6, 0.7] }, { name: "Dehradun", rainfall: [12, 5.6, 3.1, 0.55, 11, 16.2, 19] }, { name: "Nanital", rainfall: [8, 1.4, 0.61, 15.5, 6.6, 2, 9.82] }, ]
-let sum = 0;
-let count = 0;
-for(let  i =0 ; i<arr.length; i++){
-    for(let j = 0; j<arr[i].rainfall; j++){
-        sum = sum+rainfall[j];
-        count = count+1;
+// let arr = [ { name: "Delhi", rainfall: [2.3, 4, 3.1, 5.5, 1.1, 1.2, 7] }, { name: "Noida", rainfall: [6.3, 0, 0.1, 3.5, 1, 2.6, 0.7] }, { name: "Dehradun", rainfall: [12, 5.6, 3.1, 0.55, 11, 16.2, 19] }, { name: "Nanital", rainfall: [8, 1.4, 0.61, 15.5, 6.6, 2, 9.82] }, ]
+
+function rainDance(arr){
+    let ans = [];
+    for(let i = 0; i<arr.length; i++){
+        let Obj = {};
+        Obj.name = arr[i].name;
+        let sum = 0;
+        let rainfallArr = arr[i].rainfall;
+        for(let j = 0; j<rainfallArr.length; j++){
+            sum = sum + rainfallArr[j];
+        }
+        let avg = sum/rainfallArr.length;
+        Obj.avg = avg;
+        ans.push(Obj);
     }
-    console.log(sum)
+    return ans;
 }
-// [ { name: "Roorkee", rainfall: [5, 6, 5, 5, 4, 7, 8] }, { name: "Pauri", rainfall: [3, 3, 3, 1, 2, 2, 2] }, ];
-// function rainDance(rainfall) {
-//     let reqAns = [];
-//     for (x in rainfall) {
-//       let cityObj = { name: rainfall[x].name };
-//       let currCityRainfall = rainfall[x].rainfall;
-//       let rainfallSum = 0;
-//       for(x in currCityRainfall) rainfallSum+= currCityRainfall[x]
-//       let avgRainfall =
-//         rainfallSum / currCityRainfall.length;
-//       cityObj.avgRainfall = avgRainfall;
-//       reqAns.push(cityObj);
-//     }
-//     return reqAns;
-//   }
-//   let ans = rainDance();
-//   console.log(ans);
+console.log(rainDance([ { name: "Delhi", rainfall: [2.3, 4, 3.1, 5.5, 1.1, 1.2, 7] }, { name: "Noida", rainfall: [6.3, 0, 0.1, 3.5, 1, 2.6, 0.7] }, { name: "Dehradun", rainfall: [12, 5.6, 3.1, 0.55, 11, 16.2, 19] }, { name: "Nanital", rainfall: [8, 1.4, 0.61, 15.5, 6.6, 2, 9.82] }, ]));
 
 // Q4
 // let arr = [1, 2, 3]; 
